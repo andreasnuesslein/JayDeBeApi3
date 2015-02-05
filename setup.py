960357d98463ce9e80d4536bdd0a590050ba1a56
Andreas Nüßlein <andreas.nuesslein@amnesty.de>
2015-02-05 13:01:53 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='JayDeBeApi3',
+    version='1',
+    packages=['jaydebeapi'],
+    package_dir={'': 'src'},
+    url='https://github.com/nutztherookie/JayDeBeApi3',
+    license='LGPL',
+    author='Andreas Nüßlein',
+    author_email='andreas.nuesslein@amnesty.de',
+    install_requires=[
+      'JPype1-py3 >= 0.5.5.2',
+    ],
+    description='JDBC API for Python3',
+    classifiers= [
+        'Intended Audience :: Developers',
+        'Programming Language:: Java',
+        'Programming Language:: Python',
+        'Programming Language:: Python:: 3',
+    ]
+)
